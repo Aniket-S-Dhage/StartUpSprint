bed77f3bed56a5f3b6d6cd13ad635450820a4984
shubham <shubhamwable0198@gmail.com>
2024-09-30 22:31:26 +0530

    final commit

--- StartupSprint/StartupSprint/settings.py
+++ StartupSprint/StartupSprint/settings.py
@@ -193,8 +193,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "sswjk55@gmail.com"
-EMAIL_HOST_PASSWORD = 'ioie gnpl hfrw vslt'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 
